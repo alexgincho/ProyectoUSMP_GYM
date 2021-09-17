36fe25d6adb328605822a5ae4087e10a5d89e789
Alexander Chancayauri. Gincho <ginchoalexch@gmail.com>
2021-09-17 01:25:49 -0500

    Creando la conexion a la base de datos por EF Scanffold

---
+++ ProyectoUSMP_GYM/Models/ModelDB/Categorium.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProyectoUSMP_GYM.Models.Modeldb
+{
+    public partial class Categorium
+    {
+        public Categorium()
+        {
+            Productos = new HashSet<Producto>();
+        }
+
+        public int PkCategoria { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Producto> Productos { get; set; }
+    }
+}
