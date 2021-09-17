70ef55f9071a6775295dc6b9b1cd04a8e9bf9b46
Alexander Chancayauri. Gincho <ginchoalexch@gmail.com>
2021-09-17 01:30:42 -0500

    Creando Una servicio class Personal y un Interfaz

---
+++ ProyectoUSMP_GYM/Models/Services/Interfaces/IPersonalService.cs
@@ -0,0 +1,13 @@
+﻿using ProyectoUSMP_GYM.Models.Modeldb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoUSMP_GYM.Models.Services.Interfaces
+{
+    public interface IPersonalService
+    {
+        public Personaladm Create(Personaladm entity); // Creacion de un Personal Administrativo.
+    }
+}
---
+++ ProyectoUSMP_GYM/Models/Services/PersonalService.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoUSMP_GYM.Models.Modeldb;
+using ProyectoUSMP_GYM.Models.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoUSMP_GYM.Models.Services
+{
+    public class PersonalService : IPersonalService
+    {
+        public Personaladm Create(Personaladm entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
