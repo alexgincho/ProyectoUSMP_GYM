e55223c7bbd7be9df40a5cb7b4f81453fc50f9ab
Jhonatan Rivas <jjrc.12@outlook.com>
2021-10-09 04:19:03 -0500

    ProducController-ProducMantenimiento-JSProduc

--- ProyectoUSMP_GYM/wwwroot/js/Producto/Index.js
+++ ProyectoUSMP_GYM/wwwroot/js/Producto/Index.js
@@ -1,23 +1,26 @@
-﻿$(document).ready(function () {
+$(document).ready(function() {
 
-    let TablePersonal = $("#TablePersonal");
-    let btnAddPersonal = $("#btnAddPersonal");
+    let TableProducto = $("#TableProducto");
+    let btnAddProducto = $("#btnAddProducto");
 
-    btnAddPersonal.on("click", function (e) {
+    btnAddProducto.on("click", function (e) {
         e.preventDefault();
         InvocarModal();
     });
-    // Funcion Invocar un Modal
+    
+    //Función Invocar un modal 
     function InvocarModal(id) {
-        AbrirModal(`/Personal/MantenimientoPersonal/${id ? id : ""}`);
+        AbrirModal(`/Producto/MantenimientoProducto/${id ? id : ""}`);
+
     }
-    // Funcion para Abrir un Modal
+     
+    //Función para abrir un modal
     function AbrirModal(url) {
 
         $.ajax({
             type: 'GET',
             url: url,
-            dataType: "html",
+            dataType: "html", 
             cache: false,
             success: function (data) {
                 $('.modal-container').html(data).find('.modal').modal({
@@ -26,17 +29,18 @@
             }
         });
     }
-    // Listado de Personal Consumiendo Datataables
-    let DataTablePersonal = TablePersonal.DataTable({
-        scrollY: 200,
+
+    //Listadao de Producto-revisar getallproducto
+    let DataTableProducto = TableProducto.DataTable({
+        scrollY:200,
         scrollX: true,
         paging: false,
-        ordering:false,
+        ordering: false,
         ajax: {
-            url: '/Personal/GetAllPersonal',
+            url: '/Producto/GetAllProducto',
         },
         columnDefs: [
-            { targets: 0, width: 100},
+            { targets: 0, width:100},
             { targets: 1, width: 110},
             { targets: 2, width: 180 },
             { targets: 3, width: 180 },
@@ -45,19 +49,17 @@
             { targets: 6, width: 210 },
             { targets: 7, width: 150 },
             { targets: 8, width: 100 },
-            { targets: 9, width: 110 }
         ],
         columns: [
-            { data: "dni", title:"Dni" },
+            { data: "codigo", title:"Codigo" },
             { data: "nombre", title: "Nombre" },
-            { data: "apellidopaterno", title: "Apellido Paterno" },
-            { data: "apellidomaterno", title: "Apellido Materno" },
-            { data: "direccion", title: "Direccion" },
-            { data: "telefono", title:"Celular" },
-            { data: "email", title: "Email" },
-            { data: "fechacrea", title: "Fecha de Ingreso" },
-            { data: "fkRol", title: "Cargo" },
-            { data: "usuario", title:"Usuario Login" },
+            { data: "descripcion", title: "Descripcion" },
+            { data: "precioventa", title: "Precio de Venta" },
+            { data: "preciocompra", title: "Precio de Compra" },
+            { data: "cantidad", title:"Cantidad" },
+            { data: "descuento", title: "Descuento" },
+            { data: "categoria", title: "Categoria" },
+            { data: "fechavencimiento", title: "Fecha de Vencimiento" },
             {
                 data: null,
                 defaultContent: "<button type='button' id='btnEditar' class='btn btn-primary'><i class='fas fa-pen-square'></i></i></button>",
@@ -67,83 +69,87 @@
             },
             { data: null, defaultContent: "<button type='button' id='btnEliminar' class='btn btn-danger'><i class='fas fa-trash-alt'></i></i></button>"}
         ]
-    });
-    // Agregar Personal Administrativo
+    })
+
+    //Agregar Producto 
     $(".modal-container").on("click", "#btnSave", function (e) {
         e.preventDefault();
-        let Personal = {
+        let Producto = {
 
-            "Dni": $("#Dni").val(),
+            "Codigo": $("#Codigo").val(),
             "Nombre": $("#Nombre").val(),
-            "Apellidopaterno": $("#Apellidopaterno").val(),
-            "Apellidomaterno": $("#Apellidomaterno").val(),
-            "Telefono": $("#Telefono").val(),
-            "Direccion": $("#Direccion").val(),
-            "Email": $("#Email").val(),
-            "FkRol": $("#FkRol").val(),
-            "Usuario": $("#Usuario").val(),
-            "Passwords": $("#Passwords").val()
+            "Descripcion": $("#Descripcion").val(),
+            "Precioventa": $("#Precioventa").val(),
+            "Preciocompra": $("#Preciocompra").val(),
+            "Cantidad": $("#Cantidad").val(),
+            "Descuento" : $("#Descuento").val(),
+            "Categoria": $("#Categoria"),
+            "Fechavencimiento": $("#Fechavencimiento").val()
         }
 
         Swal.fire({
-            title: 'Desea Registrar a este Personal?',
+            title: 'Desea Registrar este producto?',
             showDenyButton: true,
             confirmButtonText: 'Registrar',
-            denyButtonText: `Cancelar`,
+            denyButtonText: 'Cancelar',
             denyButtonClass: 'button-cancel'
-        }).then((result) => {
-            if (result.isConfirmed) {
+        }).then((result) =>{
+            if(result.isConfirmed){
                 $.ajax({
-                    url: '/Personal/CreatePersonal',
-                    data: JSON.stringify(Personal),
+                    url: '/Producto/CreateProducto',
+                    data: JSON.stringify(Producto),
                     type: 'POST',
-                    contentType: "application/json;charset=utf-8",
+                    contentType: 'application/json;charset=utf-8',
                     dataType: "json",
                     success: function (data) {
                         console.log(data);
-                        if (data.state == 200) {
+                        if(data.state == 200){
                             console.log(data);
                             Swal.fire('Saved!', '', 'success')
                             $('#modal-default').modal('hide');
-                             DataTablePersonal.ajax.reload();
+                            DataTablePersonal.ajax.reload();
                         }
-                        else if (data.state == 404) {
+                        else if(data.state == 404){
                             console.log(data);
                             Swal.fire(`Upss! ${data.message}`, '', 'info')
                             $('#modal-default').modal('hide');
                         }
                     },
-                    error: function (error) {
+                    error: function(error) {
                         if (error.status === 400) {
-                            Swal.fire('Upss! Ocurrio Algo.', '', 'info')
+                            Swal.fire('Upss! Ocurrio Algo.....', '', 'info')
                         }
                     }
                 });
             }
-            else if (result.isDenied) {
-                Swal.fire('Cambios no Registrados', '', 'info')
-                $('#modal-default').modal('hide');
+            else if (result.isDenied){
+                Swal.fire('Los cambios no se registraron', '', 'info')
+                $('#modal-default').modal('hide');   
             }
         })
     });
 
-    // Ingresando Update
 
-    TablePersonal.on("click", "#btnEditar", function () {
-        let id = DataTablePersonal.row($(this).parents("tr")).data().pkPersonal;
+    //Ingresando Actualizar
+
+    TableProducto.on("click", "#btnEditar", function() {
+        let id = DataTableProducto.row($(this).parents("tr")).data.pkProducto;
         console.log(id);
         InvocarModal(id);
     });
+
+    //Desactiva un Producto
     
-    // Desactivar un Personal.
-    TablePersonal.on("click", "#btnEliminar", function () {
-        let id = DataTablePersonal.row($(this).parents("tr")).data().pkPersonal;
-        let nombre = DataTablePersonal.row($(this).parents("tr")).data().nombre;
+    TableProducto.on("click", "#btnEliminar", function (){
+        let id = DataTableProducto.row($(this).parents("tr")).data().pkProducto;
+        let nombre = DataTableProducto.row($(this).parents("tr")).data().nombre;
         console.log(id); console.log(nombre);
-        // Ajax consumir rest metodo delete personal.
+        
+        //Ajax
+
         Swal.fire({
-            title: 'Estas Seguro?',
-            text: `que desea eliminar a este personal : ${nombre} `,
+            title: 'Estas completamente Seguro?',
+            text: `que desea eliminar a este producto : ${nombre} `,
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
@@ -152,7 +158,7 @@
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajax({
-                    url: `/Personal/DesactivePersonal`,
+                    url: `/Producto/DesactiveProducto`,
                     data: JSON.stringify(id),                 
                     type: 'POST',
                     contentType: "application/json;charset=utf-8",
@@ -162,7 +168,7 @@
                         if (data.state == 200) {
                             Swal.fire(
                                 'Desactivado!',
-                                `El Personal ${nombre} a sido Desactivado.`,
+                                `El Producto ${nombre} a sido Desactivado.`,
                                 'success'
                             )
                             DataTablePersonal.ajax.reload();
@@ -176,7 +182,8 @@
                 });
             }
         });
-
+   
     });
+    
 
 });
