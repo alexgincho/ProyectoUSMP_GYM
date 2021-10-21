e788e5d110f8aec32c64045f8635e76c2275b7a2
Melissa <melissa_parra@usmp.pe>
2021-10-21 00:41:51 -0500

    Arreglos

--- ProyectoUSMP_GYM/Models/Services/Interfaces/IProveedorService.cs
+++ ProyectoUSMP_GYM/Models/Services/Interfaces/IProveedorService.cs
@@ -8,7 +8,7 @@ namespace ProyectoUSMP_GYM.Models.Services.Interfaces
 {
     public interface IProveedorService
     {
-        public void Create(Proveedor entity); //creacion 
+        public Proveedor Create(Proveedor entity); //creacion 
         public Proveedor Update(Proveedor entity); // actualizar 
         public List<Proveedor> GetAll(); //lista
         public bool Delete(int id); // eliminar
