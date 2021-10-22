f2dab916afe8ee0bced10f199337384a450ac094
elias arpasi <arpasiverasteguigerald@gmail.com>
2021-10-22 00:48:29 -0500

    VISTA PARA USUARIO

---
+++ ProyectoUSMP_GYM/Controllers/WebUserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoUSMP_GYM.Models.ModelDB;
+using ProyectoUSMP_GYM.Models.Response;
+using ProyectoUSMP_GYM.Models.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoUSMP_GYM.Controllers
+{
+    public class WebUserController : Controller
+    {
+         public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
