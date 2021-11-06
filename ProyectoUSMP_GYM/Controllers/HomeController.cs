4632de299a7df60626246264870cee6584c0620d
Melissa <melissa_parra@usmp.pe>
2021-11-05 20:08:22 -0500

    Rectificacion de Controladores

--- ProyectoUSMP_GYM/Controllers/HomeController.cs
+++ ProyectoUSMP_GYM/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProyectoUSMP_GYM.Models;
 using System;
@@ -6,19 +7,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ProyectoUSMP_GYM.Models.Services.Interfaces;
+using ProyectoUSMP_GYM.Models.Response;
 
 namespace ProyectoUSMP_GYM.Controllers
 {
     // prueba
-    public class HomeController : Controller
+    public class HomeController : Controller 
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private IProductoService _sProduct;
+        private IUsuarioService _Us;
+        private readonly ILogger<HomeController> _loger;
+
+
+        public HomeController(IProductoService product, IUsuarioService Us, ILogger<HomeController> loger)
         {
-            _logger = logger;
+            this._sProduct = product;
+            this._Us = Us;
+            this._loger = loger;
         }
 
+       
         public IActionResult Index()
         {
             return View();
@@ -28,11 +38,74 @@ namespace ProyectoUSMP_GYM.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Equipo()
+        {
+            return View();
+        }
+     
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateUsuario([FromBody] UsuarioRequest users)
+        {
+            Response rpta = new Response();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = _Us.CreateUsuario(users);
+                    if (user != null)
+                    {
+                        rpta.Data = user;
+                        rpta.State = 200;
+                        rpta.Message = "Exito!";
+                    }
+                    else { throw new Exception("Error."); }
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                rpta.Message = ex.Message;
+                rpta.Data = null;
+                rpta.State = 400;
+            }
+
+            return Ok(rpta);
+        }
+
+        // GET: VistaProducto
+        public ActionResult VistaProducto()
+        {
+            ViewBag.Productos = _sProduct.GetAll();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetProductoxCategoria(int id)
+        {
+
+            return Ok();
+        }
+        public IActionResult WebUser()
+        {
+            return View();
+        }
+
+        public IActionResult probando()
+        {
+            return View();
+        }
     }
 }
+
