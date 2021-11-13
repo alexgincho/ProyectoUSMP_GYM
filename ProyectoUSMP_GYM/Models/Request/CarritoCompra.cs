7adad93d639c6fa0791ddea6aca4fa0c79fc8d96
Alexander Chancayauri. Gincho <ginchoalexch@gmail.com>
2021-11-13 02:39:37 -0500

    Agregando Carrito :'v

---
+++ ProyectoUSMP_GYM/Models/Request/CarritoCompra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoUSMP_GYM.Models.Request
+{
+    public class CarritoCompra
+    {
+        public string Codigo { get; set; }
+        public double Total { get; set; }
+        public List<Detalle> Detalles { get; set; }
+
+    }
+    public class Detalle
+    {
+        public int pkProducto { get; set; }
+        public double PrecioUnitario { get; set; }
+        public int Cantidad { get; set; }
+        public double SubTotal { get; set; }
+    }
+}
